4343db6801ff3c60b91e429ad7781213c246bbbf
Andrew Root <root.drew27@gmail.com>
2024-10-03 11:20:01 -0500

    data analysis. add exp8

--- official/experiments2.0/exp8/exp8_catb_cv.py
+++ official/experiments2.0/exp8/exp8_catb_cv.py
@@ -1,6 +1,10 @@
+# This experiment aggregates the 3 datasets and runs CV 
+
+# This file uses the CatBoostClassifier with CountVectorizer AND StratifiedShuffleSplit 
+
 from catboost import CatBoostClassifier
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import StratifiedKFold, train_test_split, cross_validate
+from sklearn.model_selection import StratifiedShuffleSplit, train_test_split, cross_validate
 
 from nltk.stem.snowball import SnowballStemmer
 # from nltk.stem.porter import PorterStemmer
@@ -15,12 +19,16 @@ import json
 import os
 import sys
 import logging
+import traceback
 
 RANDOM_SEED = 115
 random.seed(RANDOM_SEED)
 np.random.seed(RANDOM_SEED)
 
-TRAIN_DATA_NAME = '48000_cyberbullying_tweets_basic_clean.csv' 
+D1_NAME = '48000_cyberbullying_tweets_basic_clean.csv' 
+D2_NAME = 'hatespeech_tweets_basic_clean.csv'
+D3_NAME = 'onlineHarassmentDataset_basic_clean.csv'
+
 FNAME = os.path.basename(__file__).split('.')[0]
 
 logging.basicConfig(
@@ -35,20 +43,43 @@ logging.basicConfig(
 
 if __name__ == '__main__':
     try:
-        logging.info(f'Starting: {FNAME}\nTrain data name: {TRAIN_DATA_NAME}\nRandom seed: {RANDOM_SEED}')
+        logging.info(f'Starting: {FNAME}\nRandom seed: {RANDOM_SEED}')
 
         data_path = Path(sys.argv[1])
 
-        train_data_path = data_path / TRAIN_DATA_NAME
+        d1_path = data_path / D1_NAME
+        d2_path = data_path / D2_NAME
+        d3_path = data_path / D3_NAME
 
-        train_df = pd.read_csv(train_data_path)
+        d1_df = pd.read_csv(d1_path, usecols=['tweet', 'label'])
+        d2_df = pd.read_csv(d2_path, usecols=['tweet', 'class'])
+        d3_df = pd.read_csv(d3_path, usecols=['Tweet', 'Code'])
+
         # remove duplicates
-        train_df = train_df.drop_duplicates()
-        # remove rows with missing values
-        train_df = train_df.dropna()
+        d1_df = d1_df.drop_duplicates()
+        d2_df = d2_df.drop_duplicates()
+        d3_df = d3_df.drop_duplicates()
 
-        train_df.reset_index(drop=True, inplace=True)
-        train_df['label'] = train_df['label'].apply(lambda label : 0 if label == 'notcb' else 1)
+        # remove rows with missing values
+        d1_df = d1_df.dropna()
+        d2_df = d2_df.dropna()
+        d3_df = d3_df.dropna()
+
+        # reset indices
+        d1_df.reset_index(drop=True, inplace=True)
+        d2_df.reset_index(drop=True, inplace=True)
+        d3_df.reset_index(drop=True, inplace=True)
+
+        # rename columns
+        d1_df.rename(columns={'tweet': 'tweet', 'label': 'label'}, inplace=True)
+        d2_df.rename(columns={'tweet': 'tweet', 'class': 'label'}, inplace=True)
+        d3_df.rename(columns={'Tweet': 'tweet', 'Code': 'label'}, inplace=True)
+
+        d1_df['label'] = d1_df['label'].apply(lambda label: 0 if label == 'notcb' else 1)
+        d2_df['label'] = d2_df['label'].apply(lambda label: 1 if label == 0 else 0)
+        d3_df['label'] = d3_df['label'].apply(lambda label: 1 if label == 'H' else 0)
+        # concatenate the datasets
+        train_df = pd.concat([d1_df, d2_df, d3_df])
 
         # split data into train and validation sets
         x_train, x_val, y_train, y_val = train_test_split(
@@ -61,13 +92,9 @@ if __name__ == '__main__':
         )
 
         snowballer = SnowballStemmer('english')
-        # porter = PorterStemmer()
 
         def pp_SnowballStemmer(text):
             return ' '.join([snowballer.stem(word) for word in text.split()])
-
-        # def pp_PorterStemmer(text):
-        #     return ' '.join([porter.stem(word) for word in text.split()])
 
         catb_param_grid = {
             'vectorizer': [CountVectorizer],
@@ -84,9 +111,11 @@ if __name__ == '__main__':
         iters = (v for _, v in catb_param_grid.items())
         keys = catb_param_grid.keys()
         param_sets = [params for params in itertools.product(*iters)]
+
         # calculate the scale_pos_weight
         pos_weight = (y_train == 0).sum() / (y_train == 1).sum()
         logging.info(f'The positive weight is {pos_weight:.4f}\n')
+
         all_results = {
             'fit_time_mean': [],
             'fit_time_std_dev': [],
@@ -97,7 +126,7 @@ if __name__ == '__main__':
             'params': []
         }
 
-        N_SPLITS = 6
+        N_SPLITS = 10
 
         logging.info(f'Starting CV for {len(param_sets)} folds (i.e. {len(param_sets) * N_SPLITS} splits)')
         for idx, params in enumerate(param_sets):
@@ -115,16 +144,22 @@ if __name__ == '__main__':
                 if 'classifier__' in k:
                     p_name = k.split('__')[1]
                     classifier_params[p_name] = t[k]
-
+            
             vect = t['vectorizer'](**vect_params)
-            classifier = t['classifier'](early_stopping_rounds=10, eval_metric='Logloss', scale_pos_weight=pos_weight, **classifier_params)
+            classifier:CatBoostClassifier = t['classifier'](
+                early_stopping_rounds=10,
+                eval_metric='Logloss',
+                scale_pos_weight=pos_weight,
+                **classifier_params,
+                thread_count=-1
+            )
 
             x_train_copy = vect.fit_transform(x_train.copy())
             x_val_copy = vect.transform(x_val.copy())
             y_train_copy = y_train.copy()
             y_val_copy = y_val.copy()
 
-            skf = StratifiedKFold(n_splits=N_SPLITS, shuffle=True, random_state=RANDOM_SEED)
+            skf = StratifiedShuffleSplit(n_splits=N_SPLITS, test_size=0.2, random_state=RANDOM_SEED)
 
             cv_results: dict = cross_validate(
                 classifier,
@@ -135,7 +170,7 @@ if __name__ == '__main__':
                 verbose=10,
                 return_estimator=False,
                 params={'eval_set':[(x_val_copy, y_val_copy)]},
-                n_jobs=-1
+                n_jobs=1
             )
 
             logging.info(f'The cv results for run {idx} are:\n{pd.DataFrame(cv_results)}\n')
@@ -158,5 +193,7 @@ if __name__ == '__main__':
 
         logging.info(f'Finished running folds. See the results in {FNAME}__results.json')
     except Exception as e:
-        logging.error(f'Error: {e}')
-        exit(1)
+        logging.error(f'Error: {e}\nStack Trace: {traceback.format_exc()}')
+    finally:
+        logging.info(f'Terminating Program: {FNAME}')
+        sys.exit()
